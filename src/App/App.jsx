0a7ffea926e7883a17b5a4fe3f4e7f73e98906d2
edwintantawi <edwintantawi@gmail.com>
2021-04-25 12:37:26 +0700

    remove unused text

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Navbar from 'components/Navbar';
 
 import './App.css';
+import Home from 'Pages/Home';
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+      <Home />
     </div>
   );
 };
-
+// some backdrop path
+// https://image.tmdb.org/t/p/w500/h4MNLYzr6Cr02iHv3Hpqb9lmTPv.jpg
 export default App;
