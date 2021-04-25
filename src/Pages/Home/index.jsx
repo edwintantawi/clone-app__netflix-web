6ad3ef5517405b947805827d6a9badccc9264895
edwintantawi <edwintantawi@gmail.com>
2021-04-25 12:36:18 +0700

    added Home

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import Hero from 'components/Hero';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Hero />
+    </div>
+  );
+};
+
+export default Home;
