45d8900eb84bef3697f951eb6ba9bd4e323fff94
edwintantawi <edwintantawi@gmail.com>
2021-04-25 10:57:26 +0700

    initial project & cleanup

---
+++ src/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Netflix web Clone</h1>
+    </div>
+  );
+};
+
+export default App;
